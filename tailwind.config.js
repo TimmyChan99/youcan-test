211cc6e0ed14d24ae24e3adbbdeb99dc470bd392
Fatima Ezzahra <felemenoun@gmail.com>
2023-03-30 15:07:07 +0000

    Create the header and navigation and implement styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,14 @@
 module.exports = {
   content: ['./src/*.html', './dist/*.html', "./src/**/*.js", "./src/**/*.jsx"],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      colors: {
+        'custom-purple': '#5E60CE',
+        'custom-blue': '#141C29',
+        'custom-gray': '#767A85',
+        'custom-gray-light': '#F9FAFC',
+      },
+    },
+    plugins: [],
+  }
 }
-
